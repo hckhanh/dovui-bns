c13a14005d57350f845b9d0f192e3980eb0a0828
Khanh Hoang <hoangchaukhanh93@gmail.com>
2018-04-27 12:29:01 +0700

    Improve the way to read questions

--- src/utils.js
+++ src/utils.js
@@ -1,7 +1,9 @@
 export function extractQuestion(content) {
-  return content
-    .split('\n')
-    .map(line => line.trim())
-    .filter(line => line.endsWith('?') === true)
+  const line = content.split('\n').map(line => line.trim())
+  const markIndex = line.indexOf('CÂU HỎI')
+  const questionContents = line.slice(markIndex + 1, markIndex + 11)
+
+  return questionContents
+    .filter((line, index) => index % 2 !== 0)
     .map(question => ({ question }))
 }
