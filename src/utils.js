29d23c860a3e926392a8ce2b2c2429a9e78f3619
Khanh Hoang <hoangchaukhanh93@gmail.com>
2018-04-27 12:35:29 +0700

    Add a solution for manual content

--- src/utils.js
+++ src/utils.js
@@ -1,9 +1,16 @@
 export function extractQuestion(content) {
   const line = content.split('\n').map(line => line.trim())
   const markIndex = line.indexOf('CÂU HỎI')
-  const questionContents = line.slice(markIndex + 1, markIndex + 11)
 
-  return questionContents
-    .filter((line, index) => index % 2 !== 0)
-    .map(question => ({ question }))
+  if (markIndex > -1) {
+    const questionContents = line.slice(markIndex + 1, markIndex + 11)
+
+    return questionContents
+      .filter((line, index) => index % 2 !== 0)
+      .map(question => ({ question }))
+  } else {
+    return line
+      .filter(line => line.endsWith('?') === true)
+      .map(question => ({ question }))
+  }
 }
