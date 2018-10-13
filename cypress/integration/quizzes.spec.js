9c968e728c30901dab94dbfa09f5e8614bf71482
Khanh Hoang <hoangchaukhanh93@gmail.com>
2018-10-13 15:52:53 +0700

    Work around for the test cases on disabled button!!!

--- cypress/integration/quizzes.spec.js
+++ cypress/integration/quizzes.spec.js
@@ -6,28 +6,28 @@ context('Quizzes', function() {
   describe('Get quizzes', function() {
     it('should get answers from the input questions (type 1)', function() {
       cy.fixture('quizzes_type_1.txt').then(function(questions) {
-        cy.get('.form-control').invoke('val', questions).trigger('change')
+        cy.get('.form-control').invoke('val', questions).trigger('change').type('{enter}')
       })
 
-      cy.get('.btn').contains('Lấy câu trả lời').click()
+      cy.get('.btn').contains('Lấy câu trả lời').click({ force: true })
       cy.get('.list-group .list-group-item').should('have.length', 5)
     })
 
     it('should get answers from the input questions (type 2)', function() {
       cy.fixture('quizzes_type_2.txt').then(function(questions) {
-        cy.get('.form-control').invoke('val', questions).trigger('change')
+        cy.get('.form-control').invoke('val', questions).trigger('change').type('{enter}')
       })
 
-      cy.get('.btn').contains('Lấy câu trả lời').click()
+      cy.get('.btn').contains('Lấy câu trả lời').click({ force: true })
       cy.get('.list-group .list-group-item').should('have.length', 5)
     })
 
     it('should display no answer component when the answer is not found', function() {
       cy.fixture('no_answer_quiz.txt').then(function(questions) {
-        cy.get('.form-control').invoke('val', questions).trigger('change')
+        cy.get('.form-control').invoke('val', questions).trigger('change').type('{enter}')
       })
 
-      cy.get('.btn').contains('Lấy câu trả lời').click()
+      cy.get('.btn').contains('Lấy câu trả lời').click({ force: true })
 
       cy.get('.list-group .list-group-item .text-danger')
         .contains('Câu hỏi này chưa có đáp án!')
