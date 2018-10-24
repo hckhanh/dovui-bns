84c9ab0c7ed9e988e50ca1640fc9d21287aa3677
Khanh Hoang <hoangchaukhanh93@gmail.com>
2018-10-24 14:56:41 +0700

    Use StrictMode component

--- src/index.js
+++ src/index.js
@@ -1,8 +1,13 @@
-import React from 'react'
+import React, { StrictMode } from 'react'
 import ReactDOM from 'react-dom'
 import App from './App'
 import './index.css'
 import registerServiceWorker from './registerServiceWorker'
 
-ReactDOM.render(<App />, document.getElementById('root'))
+ReactDOM.render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+  document.getElementById('root'))
+
 registerServiceWorker()
